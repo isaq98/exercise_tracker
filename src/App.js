083474b6d022790ffdaaaa496e3fddb892ab6c27
Sameer <shisaq@dons.usfca.edu>
2020-12-05 18:51:33 -0800

    All routes working properly with navbar

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from './components/navbar.component';
 import ExercisesList from './components/exercises-list.component';
-import EditExercises from './components/edit-exercise.component';
+import EditExercises from './components/edit-exercises.component';
 import CreateExercise from './components/create-exercise.component';
 import CreateUser from './components/create-user.component';
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Route path="/" exact component={ExercisesList}/>
-      <Route path="/edit/:id" component={EditExercises}/>
-      <Route path="/create" component={CreateExercise}/>
-      <Route path="/user" component={CreateUser}/>
+      <div className="container">
+        <Navbar />
+        <Route path="/" exact component={ExercisesList}/>
+        <Route path="/edit/:id" component={EditExercises}/>
+        <Route path="/create" component={CreateExercise}/>
+        <Route path="/user" component={CreateUser}/>
+      </div>
     </Router>
   );
 }
