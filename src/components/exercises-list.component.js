397dbcf1a6e60efdde824b757f1b6c92e46791c7
Sameer <shisaq@dons.usfca.edu>
2020-12-09 18:24:05 -0800

    Connected frontend to backend

--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -4,10 +4,10 @@ import axios from 'axios';
 
 const Exercise = props => (
     <tr>
-        <td>{props.exercises.username}</td>
-        <td>{props.exercises.description}</td>
-        <td>{props.exercises.duration}</td>
-        <td>{props.exercises.date.substring(0,10)}</td>
+        <td>{props.exercise.username}</td>
+        <td>{props.exercise.description}</td>
+        <td>{props.exercise.duration}</td>
+        <td>{props.exercise.date.substring(0,10)}</td>
         <td>
             <Link to={"/edit/"+props.exercise._id}>edit</Link> | <a href="#" onClick={() => {props.deleteExercise(props.exercise._id)}}>delete</a>
         </td>
